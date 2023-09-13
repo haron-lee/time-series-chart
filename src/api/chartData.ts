3bd6cf88f1f60a11945ac0bbe8274229fd0779c3
haron <haronxlee@gmail.com>
2023-09-13 18:06:51 +0900

    Feat: 환경변수URL  NODE ENV에 따라 변경

--- src/api/chartData.ts
+++ src/api/chartData.ts
@@ -1,7 +1,11 @@
 import Http from 'api/http';
 import { DataType } from 'types';
 
-const searchHttp = new Http(`${process.env.REACT_APP_DEV_URL}`);
+const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_DEPLOY_URL
+    : process.env.REACT_APP_DEV_URL;
+const searchHttp = new Http(BASE_URL || '');
 
 export const getData = async (): Promise<{ [date: string]: DataType }> => {
   return await searchHttp.get<any>('/response');
